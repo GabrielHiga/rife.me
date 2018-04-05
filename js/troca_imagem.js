85e1e1b023819c4ef576d978811c30263d5fd52b
macbook2011 <macbook2011@MacBook-Pro-de-macbook2011.local>
2018-04-05 19:53:16 -0300

    miniatura de imagens
    Inclusão de miniaturas e animação.

---
+++ js/troca_imagem.js
@@ -0,0 +1,15 @@
+function img01() {
+  document.getElementById('troca_imagem'). src = "imagens/buggy.jpeg";
+}
+
+function img02() {
+  document.getElementById('troca_imagem'). src = "imagens/sag_verde.jpg";
+}
+
+function img03() {
+  document.getElementById('troca_imagem'). src = "imagens/sag_cinza.jpg";
+}
+
+function img04() {
+  document.getElementById('troca_imagem'). src = "imagens/sag_cinza.jpg";
+}
